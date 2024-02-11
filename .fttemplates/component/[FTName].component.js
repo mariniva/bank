8b78f44afb5672fa8fbb5f6f33c7f80dce312987
mariniva <ivandaevam@gmail.com>
2024-02-11 12:55:20 +0700

    Created render.service

---
+++ .fttemplates/component/[FTName].component.js
@@ -0,0 +1,12 @@
+import ChildComponent from '@/core/component/child.component';
+import renderService from "@/core/services/render.service";
+
+import template from "./[FTName].template.html";
+import styles from "./[FTName].module.scss";
+
+export class [FTName | pascalcase] extends ChildComponent{
+    render() {
+        this.element = renderService.htmlToElement(template,[],styles);
+        return this.element;
+    }
+}
