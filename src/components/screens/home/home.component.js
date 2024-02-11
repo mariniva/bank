d5c8f434dac064a3f364ebb06a0d72030ca9c08b
mariniva <ivandaevam@gmail.com>
2024-02-11 13:37:26 +0700

    Created RQuery library

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,4 +1,5 @@
 import { BaseScreen } from "@/core/component/base-screen.component";
+import { $R } from "@/core/rquery/rquery.lib";
 import renderService from "@/core/services/render.service";
 import template from "./home.template.html";
 import styles from "./home.module.scss";
@@ -9,6 +10,8 @@ export class Home extends BaseScreen{
     }
     render(){
         const element = renderService.htmlToElement(template,[],styles);
+
+        $R(element).find('h1').css('color','red');
         return element.outerHTML;
     }
 }
